3702474752e6804ce14394fdd0df634610786b78
Jiggy <jignacio2204@gmail.com>
2023-06-08 16:22:01 -0400

    metadata in Head component

--- exercises/exercise-06--provide-page-specific-metadata/pages/index.tsx
+++ exercises/exercise-06--provide-page-specific-metadata/pages/index.tsx
@@ -1,13 +1,20 @@
 // ✍️ import Head from 'next/head'
+import Head from 'next/head';
 import { ChakraProvider, Heading } from '@chakra-ui/react'
+
 
 const Home = () => {
   return (
     <ChakraProvider>
       {/* ✍️ add title and description using the Head component */}
-      <Heading w="full" textAlign="center">
-        This is our homepage content!
-      </Heading>
+      <Head>
+        <title>My Next.js Course</title>
+        <meta 
+          name="description"
+          content='An egghead.io course on how to get started with Next.js'
+        />
+      </Head>
+      <Heading width="full" textAlign="center">This is our homepage content!</Heading>
     </ChakraProvider>
   )
 }
