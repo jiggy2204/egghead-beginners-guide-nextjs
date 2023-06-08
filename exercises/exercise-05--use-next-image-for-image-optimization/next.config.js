57c39b760753b03676447c3b7c6963e9d07a4b0f
Jiggy <jignacio2204@gmail.com>
2023-06-08 16:18:49 -0400

    create nextjs config file for image optimization

---
+++ exercises/exercise-05--use-next-image-for-image-optimization/next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    images: {
+        domains: ['jiggyartcom.files.wordpress.com']
+    }
+}
--- exercises/exercise-05--use-next-image-for-image-optimization/pages/index.tsx
+++ exercises/exercise-05--use-next-image-for-image-optimization/pages/index.tsx
@@ -1,11 +1,29 @@
 // ✍️ import the Image component from 'next/image'
-
+import Image from 'next/image';
 // ✍️ import "lazar.png" statically
+import lazar from '../lazar.png';
+
+const JIGGY_ART = 'https://jiggyartcom.files.wordpress.com/2023/01/valkyrie_red_web.jpg?strip=info&w=2000'
 
 const Home = () => {
-  return {
+  return (
     /* ✍️ add the lazar image here */
-  }
+    <>
+    {/**
+     * First IMAGE - Next js auto determines w and h based on imported file (see above)
+     * Second IMAGE - url import; w and h do not determine rendered size, but are used to infer the correct aspect ratio
+     */}
+      <Image src={lazar} alt="pic of Lazar" />
+      <Image
+        src={JIGGY_ART}
+        alt="Remote pic of lazar"
+        width={250}
+        height={450}
+        />
+    </>
+  )
+
+  
 }
 
 export default Home
